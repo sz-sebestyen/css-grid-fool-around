dae73eacd283d8da55aee545f7cd30d48870b676
sz-sebestyen <szabosebestyen.mail@gmail.com>
2020-12-12 23:17:02 +0100

    Add grid template areas

--- main.js
+++ main.js
@@ -1,18 +1,25 @@
 function _load() {
 	const rootDiv = document.getElementById("root");
 
-	const numberOfGridItems = 3;
+	const numberOfGridItems = 5;
 
 	const gridItemHTML = `
 		<div
 			class="grid-item"
-		>
-		</div>
+		></div>
 	`;
 
 	for (let index = 0; index < numberOfGridItems; index++) {
 		rootDiv.insertAdjacentHTML("beforeend", gridItemHTML);
 	}
+
+	const gridItems = document.querySelectorAll(".grid-item");
+
+	const gridAreas = ["header", "nav", "main", "aside", "footer"];
+
+	gridItems.forEach(function (item, index) {
+		item.classList.add(gridAreas[index]);
+	});
 }
 
 window.addEventListener("load", _load);
